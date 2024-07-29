5a5ffd650809f5e70386569b7b57f799c93fdd2e
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-07-29 15:39:47 +0200

    courseinfo 2

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,15 +1,13 @@
+import Part from './part';
 
 
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
-
   return (
-  <div>
-    <p>{part1} {exercises1}</p>
-    <p>{part2} {exercises2}</p>
-    <p>{part3} {exercises3}</p>
-  </div>
+    <div>
+      <Part parts={part1} exercises={exercises1}/>
+      <Part parts={part2} exercises={exercises2}/>
+      <Part parts={part3} exercises={exercises3}/>
+    </div>
   )
-
-
 }
 export default Content;
---
+++ src/components/part.jsx
@@ -0,0 +1,10 @@
+const Part = ({parts, exercises}) => {
+return (
+  <div>
+{parts} {exercises}
+  </div>
+)
+
+}
+
+export default Part;
