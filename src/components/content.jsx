85c1824d5c500721c6d3aa25ff71cc507aea741d
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-07-30 21:36:57 +0200

    part1 b 1.5

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,13 +1,12 @@
-import Part from './part';
+import Part from "./part";
 
-
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
   return (
     <div>
       {parts.map((part, index) => (
         <Part key={index} name={part.name} exercise={part.exercises} />
       ))}
     </div>
-  )
-}
+  );
+};
 export default Content;
--- src/components/total.jsx
+++ src/components/total.jsx
@@ -1,9 +1,6 @@
-const Total = ({parts}) => {
+const Total = ({ parts }) => {
   const totalExo = parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <div>Number of exercises {totalExo}</div>
-  )
-}
+  return <div>Number of exercises {totalExo}</div>;
+};
 
 export default Total;
