da6db03cd0e81b0ffa3bf2c5a6b678d83212cf6a
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-08-06 12:03:50 +0200

    fix

--- part1/courseinfo/src/components/total.jsx
+++ part1/courseinfo/src/components/total.jsx
@@ -1,6 +1,6 @@
 const Total = ({ parts }) => {
-  const totalExo = parts.reduce((sum, part) => sum + part.name, 0);
-  return <div>{totalExo}</div>;
+  const totalExo = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <div>Total: {totalExo}</div>;
 };
 
 export default Total;
