25c3492a26ff0747d8b125e745c5658451d1b963
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-08-12 20:56:34 +0200

    2.12 => axios, filter, conditions OK

---
+++ part2/countries/src/App.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchCountries, setSearchCountries] = useState("");
+  const handleSearch = (event) => {
+    setSearchCountries(event.target.value);
+  };
+  const countriesToShow = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchCountries.toLowerCase())
+  );
+
+  useEffect(() => {
+    console.log("effect");
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        console.log("promise fulfilled");
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+  console.log("render", countries.length, "country");
+  return (
+    <div>
+      find countries :{" "}
+      <input
+        type="text"
+        placeholder="write here"
+        value={searchCountries}
+        onChange={handleSearch}
+      ></input>
+      <ul>
+      {countriesToShow.length === 1 ? (
+        <div>
+          <h3>{countriesToShow[0].name.common}</h3>
+          <p>Capital: {countriesToShow[0].capital}</p>
+          <p>Area: {countriesToShow[0].area}</p>
+          <p>Flag: {countriesToShow[0].flag}</p>
+
+        </div>
+      ) : countriesToShow.length > 10 ? (
+        <p>Too many matches</p>
+      ) : (
+        countriesToShow.map((country) => (
+          <li key={country.cca3}>{country.name.common}</li>
+        ))
+      )}
+    </ul>
+    </div>
+  );
+}
+
+export default App;
