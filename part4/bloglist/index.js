e00e39abc5b218bac375f5d856f17dfa84fc4731
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-09-03 15:09:57 +0200

    4.1 OK

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,6 +3,16 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+
+
+const mongoDBUrl = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
+
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -12,11 +22,11 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+
+
 
 app.get('/', (req, res) => {
   res.send('Yolo Bloglist!');
@@ -43,5 +53,4 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log('Server running on port ${PORT}')})
